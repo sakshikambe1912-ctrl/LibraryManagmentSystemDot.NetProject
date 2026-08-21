77e54b098a4206bb0a67889e37650bde200d5d21
sakshikambe1912-ctrl <sakshikambe1912@gmail.com>
2026-08-21 15:10:05 +0530

    Add files via upload

---
+++ LibraryManagmentSystemMVC/View/Book/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryMVC.Views.Book
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LibraryManagmentSystemMVC/View/Book/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryMVC.Views.Book
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
