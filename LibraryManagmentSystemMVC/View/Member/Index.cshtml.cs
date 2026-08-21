f096a5e8c8be79474720e630f84d6a17890715cf
sakshikambe1912-ctrl <sakshikambe1912@gmail.com>
2026-08-21 15:11:56 +0530

    Add files via upload

---
+++ LibraryManagmentSystemMVC/View/Member/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryMVC.Views.Member
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
