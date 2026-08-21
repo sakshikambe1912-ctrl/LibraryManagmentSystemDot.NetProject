2c9dd371563671434265694747c3005f549fe088
sakshikambe1912-ctrl <sakshikambe1912@gmail.com>
2026-08-21 15:13:13 +0530

    Add files via upload

---
+++ LibraryManagmentSystemMVC/View/Transiction/Issue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryMVC.Views.Transiction
+{
+    public class IssueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LibraryManagmentSystemMVC/View/Transiction/Return.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryMVC.Views.Transiction
+{
+    public class ReturnModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
